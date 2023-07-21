3b9f8591354ba806a074c898a25b53b1c4f09516
Iliana Chevez <ilianachevez00@gmail.com>
2023-07-21 17:10:47 -0500

    updated quoteHistory_test.dart

--- flutter_application_1/test/quoteHistory_test.dart
+++ flutter_application_1/test/quoteHistory_test.dart
@@ -1,17 +1,24 @@
-import 'dart:js_interop';
-
-import 'package:flutter/material.dart';
 import 'package:flutter_application_1/controller/quoteHistoryController.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_application_1/model/quoteHistoryModel.dart';
+import 'package:flutter_application_1/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 
 void main() {
    test('Quote History Test', () async {
+
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.web,
+  );
+
     final quoteHistoryController = QuoteHistoryController();
-    List<QuoteHistoryModel> quoteHistoryModels = await quoteHistoryController.GetQuoteHistoryModels("asdf");
+    List<QuoteHistoryModel> quoteHistoryModels = await quoteHistoryController.GetQuoteHistoryModels("adminuser");
 
     // Test case 1: Verify that quoteHistory is not null
-    expect(quoteHistoryModels.isNull, false);
+    expect(quoteHistoryModels.length > 2, true);
 
     // Test case 2: Verify that quoteHistoryItems is not empty
     expect(quoteHistoryModels.isNotEmpty, true);
