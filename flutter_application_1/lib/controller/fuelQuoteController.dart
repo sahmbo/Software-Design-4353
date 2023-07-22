58fc7969b80d78ec3ee3fddc21cd2a4a2e810989
Sameer Salman <sameersalman357@gmail.com>
2023-07-21 21:06:18 -0500

    Completed fuelQuoteController w Jackies help

--- flutter_application_1/lib/controller/fuelQuoteController.dart
+++ flutter_application_1/lib/controller/fuelQuoteController.dart
@@ -1,27 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../model/fuelQuoteModel.dart';
+
 //import 'package:flutter/material.dart';
 
 
+
+
+
 class FuelQuoteController {
+
   final FuelQuote _fuelQuote = FuelQuote(
+
     gallonsRequested: 0.0,
+
     deliveryDate: DateTime.now(),
+
     suggestedPrice: 0.0,
+
     totalAmountDue: 0.0,
-    deliveryAddress: '123 Main Street',
+
+    deliveryAddress: '',
+
   );
 
+
+
+
   void updateGallonsRequested(double value) {
+
     _fuelQuote.gallonsRequested = value;
+
   }
+
+
+
 
   void updateDeliveryDate(DateTime date) {
+
     _fuelQuote.deliveryDate = date;
+
   }
+
+
+
 
   void calculateTotalAmountDue() {
+
     _fuelQuote.totalAmountDue = _fuelQuote.gallonsRequested * _fuelQuote.suggestedPrice;
+
   }
 
+
+
+
+  void saveFuelQuoteToFirestore(String username) async {
+
+    final fuelQuoteData = {
+
+      "Username": username,
+
+      "Gallons Requested": _fuelQuote.gallonsRequested,
+
+      "Delivery Date": _fuelQuote.deliveryDate,
+
+      "Suggested Price": _fuelQuote.suggestedPrice,
+
+      "Total Amount Due": _fuelQuote.totalAmountDue,
+
+      "Delivery Address": _fuelQuote.deliveryAddress,
+
+    };
+
+
+
+
+    try {
+
+      await FirebaseFirestore.instance.collection("FuelQuotes").doc(username).set(fuelQuoteData);
+
+      // Alternatively, you can use .doc() if you want to specify a document ID: .doc("fuel_quote_id").set(fuelQuoteData);
+
+    } catch (e) {
+
+      // Handle any errors that occurred during the save operation
+
+      //print("Error saving fuel quote: $e");
+
+    }
+
+  }
+
+
+
+
   FuelQuote get fuelQuote => _fuelQuote;
+
 }
--- flutter_application_1/lib/fuelQuote.dart
+++ flutter_application_1/lib/fuelQuote.dart
@@ -1,271 +1,556 @@
 import 'package:flutter/material.dart';
+
 import 'loginPage.dart';
+
 import 'controller/fuelQuoteController.dart';
+
 import 'quoteHistoryPage.dart';
+
 import 'clientManage.dart';
 
+
+
+
 void main() {
+
   runApp(MaterialApp(
-    home: const FuelQuoteForm(),
+
+    home: const FuelQuoteForm(deliveryAddress: '',),
+
     theme: ThemeData(
+
         scaffoldBackgroundColor: const Color.fromRGBO(255, 201, 173, 0.5)),
+
   ));
+
 }
+
+
+
 
 class FuelQuoteForm extends StatefulWidget {
-  const FuelQuoteForm({super.key});
+
+  final String deliveryAddress; // Add a parameter to receive the delivery address
+
+
+
+
+  const FuelQuoteForm({required this.deliveryAddress, Key? key}) : super(key: key); // remind to change!!!!!!
+
+
+
 
   @override
+
   _FuelQuoteFormState createState() => _FuelQuoteFormState();
+
 }
 
+
+
+
 class _FuelQuoteFormState extends State<FuelQuoteForm> {
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   final FuelQuoteController _fuelQuoteController = FuelQuoteController();
 
+
+
+
   final TextEditingController _gallonsController = TextEditingController();
+
   final TextEditingController _deliveryDateController = TextEditingController();
-  final String _deliveryAddress =
-      '123 Main Street'; // Replace with the actual client profile data
+
+  //final String _deliveryAddress = '123 Main Street'; // Replace with the actual client profile data
+
+
+
 
   bool _isSignOutHovered = false;
+
   bool _isCalculateHovered = false;
 
+
+
+
   void signOut() {
+
     Navigator.pushReplacement(
+
       context,
+
       MaterialPageRoute(builder: (context) => const LoginApp()),
+
     );
+
   }
 
+
+
+
   @override
+
   void dispose() {
+
     _gallonsController.dispose();
+
     _deliveryDateController.dispose();
+
     super.dispose();
+
   }
+
+
+
 
   void calculateTotalAmountDue() {
+
     double gallons = double.tryParse(_gallonsController.text) ?? 0.0;
+
     _fuelQuoteController.updateGallonsRequested(gallons);
+
     _fuelQuoteController.calculateTotalAmountDue();
 
+
+
+
     print('Gallons: ${_fuelQuoteController.fuelQuote.gallonsRequested}');
+
     print('Total amount due: ${_fuelQuoteController.fuelQuote.totalAmountDue}');
+
   }
+
+
+
 
   @override
+
   Widget build(BuildContext context) {
+
     var suggestedPrice = 0;
+
     var totalAmountDue = 0;
+
     return Scaffold(
+
       //nav bar
+
       appBar: AppBar(
+
         title: const Text('Fuel Quote Form'),
+
         actions: <Widget>[
+
           IconButton(
+
             icon: Icon(Icons.account_circle),
+
             onPressed: () {
+
               Navigator.pushReplacement(
+
                 context,
+
                 MaterialPageRoute(
+
                   builder: (context) => ClientManagementApp(),
+
                 ),
+
               );
+
             },
+
           ),
+
           IconButton(
+
             icon: Icon(Icons.local_gas_station),
+
             onPressed: () {
+
               Navigator.pushReplacement(
+
                 context,
+
                 MaterialPageRoute(
-                  builder: (context) => FuelQuoteForm(),
+
+                  builder: (context) => FuelQuoteForm(deliveryAddress: '',),
+
                 ),
+
               );
+
             },
+
           ),
+
           IconButton(
+
             icon: Icon(Icons.history),
+
             onPressed: () {
+
               Navigator.pushReplacement(
+
                 context,
+
                 MaterialPageRoute(
+
                   builder: (context) => QuoteHistoryPage(),
+
                 ),
+
               );
+
             },
+
           ),
+
           IconButton(
+
             icon: Icon(Icons.logout),
+
             onPressed: () {
+
               Navigator.pushReplacement(
+
                 context,
+
                 MaterialPageRoute(
+
                   builder: (context) => LoginApp(),
+
                 ),
+
               );
+
             },
+
           ),
+
         ],
+
       ),
+
       //end nav bar
 
+
+
+
       body: Padding(
+
         padding: const EdgeInsets.all(16.0),
+
         child: Form(
+
           key: _formKey,
+
           child: ListView(
+
             children: [
+
               // Container(
+
               //   padding: const EdgeInsets.all(10.0),
+
               //   decoration: BoxDecoration(
+
               //     borderRadius: BorderRadius.circular(10.0),
+
               //     boxShadow: [
+
               //       BoxShadow(
+
               //         color: Colors.grey.withOpacity(0.45),
+
               //         spreadRadius: 5,
+
               //         blurRadius: 7,
+
               //         offset: const Offset(0, 3),
+
               //       ),
+
               //     ],
+
               //     gradient: const LinearGradient(
+
               //       colors: [Color(0xFF9EBC9F), Color(0xFF9EBC9F)],
+
               //       begin: Alignment.topCenter,
+
               //       end: Alignment.bottomCenter,
+
               //     ),
+
               //   ),
+
               //   child: Row(
+
               //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
               //     children: [
+
               //       Text(
+
               //         'Fuel Quote Form',
+
               //         style: TextStyle(
+
               //           fontSize: 24,
+
               //           fontWeight: FontWeight.bold,
+
               //           color: Colors.white.withOpacity(1),
+
               //         ),
+
               //       ),
+
               //       MouseRegion(
+
               //         onEnter: (_) {
+
               //           setState(() {
+
               //             _isSignOutHovered = true;
+
               //           });
+
               //         },
+
               //         onExit: (_) {
+
               //           setState(() {
+
               //             _isSignOutHovered = false;
+
               //           });
+
               //         },
+
               //         child: ElevatedButton(
+
               //           onPressed: signOut,
+
               //           style: ElevatedButton.styleFrom(
+
               //             foregroundColor: _isSignOutHovered
+
               //                 ? Colors.white
+
               //                 : const Color.fromRGBO(15, 76, 92, 1.0), backgroundColor: _isSignOutHovered
+
               //                 ? const Color.fromRGBO(255, 163, 165, 1.0)
+
               //                 : const Color.fromRGBO(255, 163, 165, 1.0),
+
               //           ),
+
               //           child: Text(
+
               //             'Sign Out',
+
               //             style: TextStyle(
+
               //               color:
+
               //                   _isSignOutHovered ? Colors.white : Colors.black,
+
               //             ),
+
               //           ),
+
               //         ),
+
               //       ),
+
               //     ],
+
               //   ),
+
               // ),
+
               TextFormField(
+
                 controller: _gallonsController,
+
                 keyboardType: TextInputType.number,
+
                 validator: (value) {
+
                   if (value?.isEmpty ?? true) {
+
                     return 'Please enter the number of gallons requested.';
+
                   }
+
                   return null;
+
                 },
+
                 decoration: const InputDecoration(
+
                   labelText: 'Gallons Requested',
+
                 ),
+
               ),
+
               TextFormField(
+
                 controller: _deliveryDateController,
+
                 keyboardType: TextInputType.datetime,
+
                 onTap: () async {
+
                   // Open a date picker and update the delivery date controller
+
                 },
+
                 validator: (value) {
+
                   if (value?.isEmpty ?? true) {
+
                     return 'Please select the delivery date.';
+
                   }
+
                   return null;
+
                 },
+
                 decoration: const InputDecoration(
+
                   labelText: 'Delivery Date',
+
                 ),
+
               ),
+
               TextFormField(
-                initialValue: _deliveryAddress,
+
+                initialValue: widget.deliveryAddress, // Use the provided delivery address here
+
                 enabled: false,
+
                 decoration: const InputDecoration(
+
                   labelText: 'Delivery Address',
+
                 ),
+
               ),
+
               TextFormField(
+
                 initialValue: suggestedPrice.toString(),
+
                 enabled: false,
+
                 decoration: const InputDecoration(
+
                   labelText: 'Suggested Price / gallon',
+
                 ),
+
               ),
+
               TextFormField(
+
                 initialValue: totalAmountDue.toString(),
+
                 enabled: false,
+
                 decoration: const InputDecoration(
+
                   labelText: 'Total Amount Due',
+
                 ),
+
               ),
+
               MouseRegion(
+
                 onEnter: (_) {
+
                   setState(() {
+
                     _isCalculateHovered = true;
+
                   });
+
                 },
+
                 onExit: (_) {
+
                   setState(() {
+
                     _isCalculateHovered = false;
+
                   });
+
                 },
+
                 child: ElevatedButton(
+
                   onPressed: () {
+
                     if (_formKey.currentState?.validate() ?? false) {
+
                       calculateTotalAmountDue();
+
                       setState(() {});
+
                     }
+
                   },
+
                   style: ElevatedButton.styleFrom(
+
                     foregroundColor: _isCalculateHovered
+
                         ? Colors.white
+
                         : const Color.fromRGBO(15, 76, 92, 1.0),
+
                     backgroundColor: _isCalculateHovered
+
                         ? const Color.fromRGBO(255, 163, 165, 1.0)
+
                         : const Color.fromRGBO(255, 163, 165, 1.0),
+
                   ),
+
                   child: Text(
+
                     'Calculate',
+
                     style: TextStyle(
+
                       color: _isCalculateHovered ? Colors.white : Colors.black,
+
                     ),
+
                   ),
+
                 ),
+
               ),
+
             ],
+
           ),
+
         ),
+
       ),
+
     );
+
   }
+
 }
